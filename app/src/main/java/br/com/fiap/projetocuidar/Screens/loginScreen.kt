66e050b3727a6cf683c79236bae2efcdecf3d897
Felipe Andrade <feandrade.work@gmail.com>
2025-08-29 18:18:46 -0300

    Commit realizado da criação da tela do projeto com o logo do projeto inserida.

--- app/src/main/java/br/com/fiap/projetocuidar/Screens/loginScreen.kt
+++ app/src/main/java/br/com/fiap/projetocuidar/Screens/loginScreen.kt
@@ -1,20 +1,50 @@
 package br.com.fiap.projetocuidar.Screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import br.com.fiap.projetocuidar.R
 
 @Composable
-fun LoginScreen(navController: NavController, modifier: Modifier = Modifier) {
+fun LoginScreen(navController: NavController.Companion, modifier: Modifier = Modifier) {
     Box(modifier = Modifier
         .fillMaxSize()
         .background(Color.White)
-        .padding(16.dp)
-    )
+        .padding(32.dp)
+    ){
+        Image(
+            painter = painterResource(R.drawable.logo,),
+            contentDescription = "Logo cuidar+",
+            modifier = Modifier
+                .size(300.dp)
+                .padding(16.dp)
+                .align(alignment = Alignment.TopCenter)
+        )
+        Button(
+            onClick = {/*TODO*/},
+            modifier = Modifier
+                .align(alignment = Alignment.Center)
+                .padding(bottom = 16.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = colorResource(R.color.cor_botao)
+            )
+        ){
+
+        }
+    }
 }
