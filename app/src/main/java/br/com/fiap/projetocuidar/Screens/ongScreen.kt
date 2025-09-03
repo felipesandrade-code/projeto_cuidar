ea90075e798ede68cf0dfa94981df0d236ccef7a
Felipe Andrade <feandrade.work@gmail.com>
2025-09-03 14:46:28 -0300

    Melhor Manipulação do Layout da Tela de cadastro de ongs

--- app/src/main/java/br/com/fiap/projetocuidar/Screens/ongScreen.kt
+++ app/src/main/java/br/com/fiap/projetocuidar/Screens/ongScreen.kt
@@ -2,9 +2,16 @@ package br.com.fiap.projetocuidar.Screens
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import br.com.fiap.projetocuidar.composables.ColumnOng
 
 @Composable
 fun OngScreen(modifier: Modifier = Modifier) {
     ColumnOng()
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun OngScreenPreview() {
+    
 }
